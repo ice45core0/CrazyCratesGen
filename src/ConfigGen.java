6b446d638a1b1dc2fccc086785cb798ffa8991f9
ice45core0 <ice45core01@gmail.com>
2021-04-11 18:40:16 -0500

    Creating a new crate/adding items to the crate.

--- src/ConfigGen.java
+++ src/ConfigGen.java
@@ -1,17 +1,20 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConfigGen {
+    static ArrayList<Item> items = new ArrayList<>();
     static Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int menu = 0;
         System.out.println("=======================================================");
         System.out.println("Crazy Crates Config Generator -- Created by ice45core0");
         System.out.println("=======================================================");
         System.out.println("Type the number for your selection and hit enter");
         do {
+            System.out.println("-------------------------------------------------------");
             System.out.println("[1]. Create new crate file");
             System.out.println("[2]. Edit existing crate file");
             System.out.println("[3]. Quit");
@@ -32,28 +35,16 @@ public class ConfigGen {
         } while (menu != 3);
     }
 
-    public static void newCrate() {
-        String type;
+    public static void newCrate() throws IOException {
         System.out.println("What would you like to name the config file? (do not include the extension, example: ___________.yml)");
         String fileName = in.nextLine();
-        try {
-            FileWriter file = new FileWriter(fileName +".yml");
+        FileWriter file = null;
+        try { 
+            file = new FileWriter(fileName +".yml");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("What would you like to name the crate?(Can use color codes): ");
-        String name = in.nextLine();
-        do {
-            System.out.println("What type of crate is it?(CSGO/QuadCrate/QuickCrate/Roulette/CrateOnTheGo/FireCracker/Wonder/Wheel/War/Cosmic)");
-            type = in.nextLine();
-            if (type != "CSGO" || type != "QuadCrate" || type != "QuickCrate" || type != "Roulette" || type != "CrateOnTheGo" || type != "FireCracker" || type != "Wonder" || type != "Wheel" || type != "War" || type != "Cosmic"){
-                System.out.println("Invalid input, please use one of the types provided.");
-            }
-        }while (type != "CSGO" || type != "QuadCrate" || type != "QuickCrate" || type != "Roulette" || type != "CrateOnTheGo" || type != "FireCracker" || type != "Wonder" || type != "Wheel" || type != "War" || type != "Cosmic");
-        System.out.println("What description would you like to give this crate?:");
-        String crateDesc = in.nextLine();
-        System.out.println("What description would you like to give the key?:");
-        String keyDesc = in.nextLine();
+        addHeader(file);
         int menu = 0;
         do{
             System.out.println("-------------------------------------------------------");
@@ -64,6 +55,7 @@ public class ConfigGen {
 
             switch (menu) {
                 case 1:
+                    addItem(file);
                     break;
                 case 2:
                     break;
@@ -74,6 +66,88 @@ public class ConfigGen {
                     break;
             }
         }while (menu != 3);
+        file.close();
+    }
+    
+    public static void addHeader(FileWriter file) throws IOException {
+        String type;
+        System.out.println("What would you like to name the crate?(Can use color codes, example: &eVote Crate): ");
+        String name = in.nextLine();
+        do {
+            System.out.println("What type of crate is it?(CSGO/QuadCrate/QuickCrate/Roulette/CrateOnTheGo/FireCracker/Wonder/Wheel/War/Cosmic)");
+            type = in.nextLine();
+            if (!type.equals("CSGO") && !type.equals("QuadCrate") && !type.equals("QuickCrate") && !type.equals("Roulette") && !type.equals("CrateOnTheGo") && !type.equals("FireCracker") && !type.equals("Wonder") && !type.equals("Wheel") && !type.equals("War") && !type.equals("Cosmic")){
+                System.out.println("Invalid input, please use one of the types provided.");
+            }
+        }while (!type.equals("CSGO") && !type.equals("QuadCrate") && !type.equals("QuickCrate") && !type.equals("Roulette") && !type.equals("CrateOnTheGo") && !type.equals("FireCracker") && !type.equals("Wonder") && !type.equals("Wheel") && !type.equals("War") && !type.equals("Cosmic"));
+        System.out.println("What description would you like to give this crate?:");
+        String crateDesc = in.nextLine();
+        System.out.println("What item is the key?(item id # or descriptor, example: 5151 or PIXELMON_TOUGA_BERRY):");
+        String keyItem = in.nextLine();
+        System.out.println("What description would you like to give the key?:");
+        String keyDesc = in.nextLine();
+        file.write("Crate:" +"\n");
+        file.write("  CrateType: " +type +"\n");
+        file.write("  CrateName: '" +name +"'" +"\n");
+        file.write("  Preview-Name: '" +name +" Preview'" +"\n");
+        file.write("  StartingKeys: 1" +"\n");
+        file.write("  InGUI: true" +"\n");
+        file.write("  Slot: 21" +"\n");
+        file.write("  OpeningBroadCast: false" +"\n");
+        file.write("  Broadcast: '&e&l%Player% &7is opening a " +name +"&7'" +"\n");
+        file.write("  Item: CHEST" +"\n");
+        file.write("  Glowing: false" +"\n");
+        file.write("  Name: '" +name +"'" +"\n");
+        file.write("  Lore:" +"\n");
+        file.write("  - '&7" +crateDesc +"'" +"\n");
+        file.write("  - '&7You have &6%Keys% keys &7to open this crate with.'" +"\n");
+        file.write("  - '&7&l(&e&l!&7&l) Right click to view rewards.'" +"\n");
+        file.write("  Preview:" +"\n");
+        file.write("    Toggle: true" +"\n");
+        file.write("    ChestLines: 5:" +"\n");
+        file.write("    Glass:" +"\n");
+        file.write("      Toggle: true" +"\n");
+        file.write("      Item: STAINED_GLASS_PANE:7" +"\n");
+        file.write("  PhysicalKey:" +"\n");
+        file.write("    Name: '" +name +" Key'" +"\n");
+        file.write("    Lore:" +"\n");
+        file.write("    - '&e" +keyDesc +"'" +"\n");
+        file.write("    - '&eBest of luck!'" +"\n");
+        file.write("    Item: '" +keyItem +"'" +"\n");
+        file.write("    Glowing: true" +"\n");
+        file.write("  Hologram:" +"\n");
+        file.write("    Toggle: true" +"\n");
+        file.write("    Height: 1.5" +"\n");
+        file.write("    Message:" +"\n");
+        file.write("    - '&l" +name +"'" +"\n");
+        file.write("  Prizes:" +"\n");
     }
 
+    public static void addItem(FileWriter file) throws IOException {
+        Item item = new Item();
+        items.add(item);
+        file.write("    " +(items.indexOf(item)+1) +":\n");
+        file.write("      DisplayName: '" +item.getDisplayName() +"'\n");
+        file.write("      DisplayItem: '" +item.getItemID() +"'\n");
+        file.write("      DisplayAmount: " +item.getAmount() +"\n");
+        file.write("      Lore:\n");
+        file.write("        - '" +item.getDesc() +"'\n");
+        file.write("        - '&c" +item.getChance() +"% chance'\n");
+        file.write("      MaxRange: 100\n");
+        file.write("      Chance: " +item.getChance() +"\n");
+        file.write("      Firework: false\n");
+        file.write("      Glowing: false\n");
+        file.write("      Player: ''\n");
+        file.write("      Unbreakable: false\n");
+        file.write("      Items:\n");
+        file.write("      Commands:\n");
+        file.write("        - '" +item.getCommand() +"'\n");
+        file.write("      Messages: []\n");
+        file.write("      BlackListed-Permissions: []\n");
+        file.write("      Alternative-Prize:\n");
+        file.write("        Toggle: false\n");
+        file.write("        Commands: []\n");
+        file.write("        Items: []\n");
+        file.write("        Messages: []\n");
+    }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Item{
     private String displayName;
     private String itemID;
@@ -6,10 +8,39 @@ public class Item{
     private double chance;
     private String command;
 
-    public Item(){}
+    public Item(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What's the name you want displayed for the reward?(Can use color codes, example: &c3 Poke Balls):");
+        displayName = in.nextLine();
+        System.out.println("What item do you want to display?(item id # or descriptor, example: 5151 or PIXELMON_TOUGA_BERRY):");
+        itemID = in.nextLine();
+        System.out.println("How many items do you want displayed?:");
+        amount = in.nextInt();
+        in.nextLine();
+        System.out.println("What is the description of this reward?:");
+        desc = in.nextLine();
+        System.out.println("What is the percent chance of landing on this reward?(without % sign):");
+        chance = in.nextDouble();
+        in.nextLine();
+        System.out.println("What command do you want to execute when they land on this reward?");
+        command = in.nextLine();
+    }
+
+    public String getDisplayName(){return displayName;}
+    public String getItemID(){return itemID;}
+    public int getAmount(){return amount;}
+    public String getDesc(){return desc;}
+    public double getChance(){return chance;}
+    public String getCommand(){return command;}
 
     public String toString(){
-
-        return null;
+        return ("======================================"
+                +("|| Item Name: " +displayName +"\n")
+                +("|| Item ID: " +itemID +"\n")
+                +("|| Amount: " +amount +"\n")
+                +("|| Description: " +desc +"\n")
+                +("|| Chance: " +chance +"%" +"\n")
+                +("|| Commands: " +command +"\n")
+                +"=====================================");
     }
 }
